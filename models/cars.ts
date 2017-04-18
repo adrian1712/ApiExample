5d1529669c8c145107b7c51ecc40f46b41d42b61
Adrian Amaya <aa17.1atq@gmail.com>
2017-04-18 16:14:45 -0700

    Added Models for cars and manufature

---
+++ models/cars.ts
@@ -0,0 +1,15 @@
+import * as mongoose from 'mongoose';
+
+export interface Car     extends mongoose.Document {
+  name: string;
+  type: string;
+  price: number
+}
+
+let carsSchema = new mongoose.Schema ({
+    name: String,
+    type: String,
+    price: Number
+})
+
+export default carsSchema;
--- models/manufactures.ts
+++ models/manufactures.ts
@@ -1,1 +1,15 @@
 import * as mongoose from 'mongoose';
+import CarModel from './cars';
+import {Car} from './cars';
+
+export interface Character extends mongoose.Document {
+  name: string;
+  cars: Car[]
+}
+
+let manufacturesSchema = new mongoose.Schema({
+    name: String,
+    popularity: String,
+    cars: [CarModel]
+
+})
